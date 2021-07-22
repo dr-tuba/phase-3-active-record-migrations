d1c369cf8c46be07e5fdaf6858c0d3569c05c00a
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-07-22 17:31:08 -0600

    commit tree 6d891974d39d7ed9f4dc97158284defd2be831d3

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,17 @@
+require 'rake'
+require 'active_record'
+require 'yaml/store'
+require 'ostruct'
+require 'date'
+
 ENV["RACK_ENV"] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
+
+ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/artists.sqlite"
+  )
 
 require_relative "../app/models/artist"
